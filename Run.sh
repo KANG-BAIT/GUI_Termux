1b8cd54fcd320a2d9c9039960e0dcef0e5b7d317
KANG BaIT <81130868+KANG-BAIT@users.noreply.github.com>
2021-03-22 10:08:12 +0700

    GUI Termux
    Install the GUI in Termux for those of you who like to feel the sensation of a computer on your Android. Good luck

---
+++ Run.sh
@@ -0,0 +1,91 @@
+
+unzip Run.zip 
+chmod +x Run/unistall.sh 
+chmod +x Run/unduh.sh 
+chmod +x Run/start.sh 
+rm -rf Run.zip
+ins=./Run/unduh.sh
+uni=./Run/unistall.sh
+st=./Run/start.sh
+Q=exit
+clear
+echo "\033[5;31m"
+echo ""
+echo "             █░▄▀ █▀▀█ █▀▀▄ █▀▀▀   █▀▀█ █▀▀█ ▀█▀ ▀▀█▀▀        "
+echo "             █▀▄░ █▄▄█ █░░█ █░▀█   █▀▀▄ █▄▄█ ░█░ ░▒█░░        "
+echo "             █░▒█ ▀░░▀ ▀░░▀ ▀▀▀▀   █▄▄█ ▀░░▀ ▄█▄ ░▒█░░        "
+printf '\033[1;33m'
+echo "  ─────────────╔╗─╔╗──────────  ───╔═╗──────  ╔╗────╔╗──────╔═╗─────────"""
+echo "  ╔═╗╔╦╗╔═╗─╔═╗║╚╗╠╣╔═╗╔═╗─╔╗─  ╔╦╗║═╣╔═╗╔╦╗  ╠╣╔═╦╗║╚╗╔═╗╔╦╗║═╣╔═╗─╔═╗╔═╗"""
+echo "  ║╬║║╔╝║╬╚╗║╬║║║║║║║═╣║╬╚╗║╚╗  ║║║╠═║║╩╣║╔╝  ║║║║║║║╔╣║╩╣║╔╝║╔╝║╬╚╗║═╣║╩╣"""
+echo "  ╠╗║╚╝─╚══╝║╔╝╚╩╝╚╝╚═╝╚══╝╚═╝  ╚═╝╚═╝╚═╝╚╝─  ╚╝╚╩═╝╚═╝╚═╝╚╝─╚╝─╚══╝╚═╝╚═╝"""
+echo "  ╚═╝───────╚╝────────────────  ────────────  ────────────────────────────"""
+echo ""
+echo "\033[5;32m"
+echo ""
+echo " _______________________________________________________________________"
+echo "|                                                                       |"
+echo "| Made By: Kang BaIT                                                    |"
+echo "| Chanel: Kang BaIT                                                     |"
+echo "| Instagram: kang_bait_01                                               |"
+echo "|_______________________________________________________________________|"
+echo ""
+echo ""
+echo "Note:  Pilih Salah Satu Nomer Untuk Menjalankan Perintah"
+echo "       Jangan Lupa Subcribe,Like,dan Share Chanel Kang BaIT Guys"
+echo ""
+echo "    ╬╬─━─━─━∞◆∞━─━─━─╬╬"
+echo "    ║║               ║║"
+echo "    ║║ (1).Install   ║║"
+echo "    ║║ (2).Start     ║║"
+echo "    ║║ (3).Unistall  ║║"
+echo "    ║║ (4).Quit      ║║"
+echo "    ║║               ║║"
+echo "    ╬╬─━─━─━∞◆∞━─━─━─╬╬"
+echo "" 
+echo "\033[1;34m"
+read -p "\$ " data
+echo "\033[1;36m"
+case $data in
+1)$ins;;
+2)$st;;
+3)$uni;;
+4)$Q;;
+*) clear
+echo "\033[5;31m" 
+echo "" 
+echo "             █░▄▀ █▀▀█ █▀▀▄ █▀▀▀   █▀▀█ █▀▀█ ▀█▀ ▀▀█▀▀        " 
+echo "             █▀▄░ █▄▄█ █░░█ █░▀█   █▀▀▄ █▄▄█ ░█░ ░▒█░░        " 
+echo "             █░▒█ ▀░░▀ ▀░░▀ ▀▀▀▀   █▄▄█ ▀░░▀ ▄█▄ ░▒█░░        " 
+printf '\033[1;33m' 
+echo "  ─────────────╔╗─╔╗──────────  ───╔═╗──────  ╔╗────╔╗──────╔═╗─────────"""
+echo "  ╔═╗╔╦╗╔═╗─╔═╗║╚╗╠╣╔═╗╔═╗─╔╗─  ╔╦╗║═╣╔═╗╔╦╗  ╠╣╔═╦╗║╚╗╔═╗╔╦╗║═╣╔═╗─╔═╗╔═╗"""
+echo "  ║╬║║╔╝║╬╚╗║╬║║║║║║║═╣║╬╚╗║╚╗  ║║║╠═║║╩╣║╔╝  ║║║║║║║╔╣║╩╣║╔╝║╔╝║╬╚╗║═╣║╩╣"""
+echo "  ╠╗║╚╝─╚══╝║╔╝╚╩╝╚╝╚═╝╚══╝╚═╝  ╚═╝╚═╝╚═╝╚╝─  ╚╝╚╩═╝╚═╝╚═╝╚╝─╚╝─╚══╝╚═╝╚═╝"""
+echo "  ╚═╝───────╚╝────────────────  ────────────  ────────────────────────────"""
+echo ""
+echo "\033[5;32m"
+echo ""
+echo " _______________________________________________________________________"
+echo "|                                                                       |"
+echo "| Made By: Kang BaIT                                                    |"
+echo "| Chanel: Kang BaIT                                                     |"
+echo "| Instagram: kang_bait_01                                               |"
+echo "|_______________________________________________________________________|"
+echo ""
+echo ""
+echo "Note:  Pilih Salah Satu Nomer Untuk Menjalankan Perintah"
+echo "       Jangan Lupa Subcribe,Like,dan Share Chanel Kang BaIT Guys"
+echo ""
+echo "    ╬╬─━─━─━∞◆∞━─━─━─╬╬"
+echo "    ║║               ║║"
+echo "    ║║ (1).Install   ║║"
+echo "    ║║ (2).Start     ║║"
+echo "    ║║ (3).Unistall  ║║"
+echo "    ║║ (4).Quit      ║║"
+echo "    ║║               ║║"
+echo "    ╬╬─━─━─━∞◆∞━─━─━─╬╬"
+echo ""
+read -p " Perintah Tidak Tersedia tolong masukan nomer yang benar!" int 
+sh Run.sh;;
+esac
